ca6ee92b9a2d17761b0e5d3e7742ff7c1c8dde7b
Pradhyumna-Joshi <pradhyumnajoshi07@gmail.com>
2023-10-27 19:53:16 +0530

    updated url

--- backend/index.js
+++ backend/index.js
@@ -21,21 +21,12 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-app.use("https://e-commerce-app-mocha-omega.vercel.app/api/auth", authRouter);
-app.use("https://e-commerce-app-mocha-omega.vercel.app/api/users", userRouter);
-app.use(
-  "https://e-commerce-app-mocha-omega.vercel.app/api/products",
-  productRouter
-);
-app.use(
-  "https://e-commerce-app-mocha-omega.vercel.app/api/orders",
-  orderRouter
-);
-app.use("https://e-commerce-app-mocha-omega.vercel.app/api/carts", cartRouter);
-app.use(
-  "https://e-commerce-app-mocha-omega.vercel.app/api/checkout",
-  stripeRouter
-);
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/carts", cartRouter);
+app.use("/api/checkout", stripeRouter);
 
 app.listen(5000, () => {
   console.log("Running on port 5000");
